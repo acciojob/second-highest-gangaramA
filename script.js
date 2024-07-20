cfa55dd1106ebc3217cd957ce094f36bd5d6c4c7
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-07-20 09:54:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) {
+        return -Infinity;
+    }
+    
+    let uniqueElements = new Set(arr);
+    
+    if (uniqueElements.size < 2) {
+        return -Infinity;
+    }
+    
+    let maxElement = Math.max(...uniqueElements);
+    uniqueElements.delete(maxElement);
+    
+    let secondMaxElement = Math.max(...uniqueElements);
+    
+    return secondMaxElement;
+}
