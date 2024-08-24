42e4bf97c69d4e20ae5ed99fcdea9107d5926d29
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-08-24 10:40:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ function secondHighest(arr) {
     if (arr.length < 2) {
         return -Infinity;
     }
-    
-    // Convert array to a set to remove duplicates
+
+    // Convert array to a set to remove duplicate
     let uniqueElements = new Set(arr);
-    
+
     // If there's less than 2 unique elements, return -Infinity
     if (uniqueElements.size < 2) {
         return -Infinity;
     }
-    
+
     // Convert set to an array and sort in descending order
     let sortedArray = Array.from(uniqueElements).sort((a, b) => b - a);
-    
+
     // Return the second element in the sorted array
     return sortedArray[1];
 }
